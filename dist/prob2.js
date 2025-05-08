0d456ba41bafe033ed87c717d521863ec5497f01
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-05-08 09:45:51 +0600

    problem 2

---
+++ dist/prob2.js
@@ -0,0 +1,16 @@
+"use strict";
+// Problem 2:
+// Description: Create a function that filters an array of objects by the rating property, returning only items with a rating of 4 or more.
+// Function Signature:
+// function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]
+function filterByRating(items) {
+    var _a;
+    return (_a = items.filter(item => item.rating > 4)) !== null && _a !== void 0 ? _a : [];
+}
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+console.log(filterByRating(books));
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
